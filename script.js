b77c661148ddb5288fb0d4d87eae83ea5ebb35c6
inmujtuba <62804688+inmujtuba@users.noreply.github.com>
2023-03-21 19:56:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function frnews() {
+	document.getElementById('frrnews').innerHTML=" '' Buy 80, 125, 250 & 500 sq. yards plots in Brand New ASF City! '' "
+}
+
+function scnews() {
+	document.getElementById('frrnews').innerHTML=" '' Plots Available on easy installments at New Shangrila City! '' "
+}
+
+function thnews() {
+	document.getElementById('frrnews').innerHTML=" '' New Listing On Properties For Sale '' "
+}
+
+
+
+function phonenumber() {
+	document.getElementById('phonen').innerHTML=" +92 3122721167 "
+}
